5a3dbf4677e230bb09b37016f520528a56f56f02
RNV <RNV@d.c>
2023-07-19 18:20:31 +0300

    1.Manu 2.Navigator

--- lib/pages/main_skrin.dart
+++ lib/pages/main_skrin.dart
@@ -1,8 +1,31 @@
+import 'dart:js';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
+
+  void _menuOpen(){
+    Navigator.of(context as BuildContext).push(
+        MaterialPageRoute(builder:(BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(title: Text('Menu'),),
+            body: Row(
+              children: [
+                ElevatedButton(onPressed: (){
+                  Navigator.pop(context);
+                  Navigator.pushNamedAndRemoveUntil(context,'/', (route) => false);
+          },
+                child: Text('NA golovny') ),
+                Padding(padding: EdgeInsets.only(left: 15)),
+                Text('Nashe proste Meny')
+              ],
+            )
+          );
+        })
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +34,9 @@ class MainScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text('Notes'),
           centerTitle: true,
+          actions: [
+            IconButton(onPressed: _menuOpen, icon: Icon(Icons.menu_outlined))
+          ],
         ),
         body: Column(
           children: [
